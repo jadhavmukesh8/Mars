e6a0c9d0bb730abee0671d99b25d6a8c6d4a77cc
jadhavmukesh8 <114101139+jadhavmukesh8@users.noreply.github.com>
2025-01-08 20:53:33 +0530

    Update App.controller.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,1 +1,13 @@
-
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.airbus.zcfe2meautorc.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
