1c9a7e411a18465578d695fbde46b11a193a55d6
jadhavmukesh8 <114101139+jadhavmukesh8@users.noreply.github.com>
2025-01-10 20:55:55 +0530

    Update Component.js

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,1 +1,37 @@
+/**
+ * eslint-disable @sap/ui5-jsdocs/no-jsdoc
+ */
 
+sap.ui.define([
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "com/airbus/zcfe2meautorc/model/models"
+    ],
+    function (UIComponent, Device, models) {
+        "use strict";
+
+        return UIComponent.extend("com.airbus.zcfe2meautorc.Component", {
+            metadata: {
+                manifest: "json",
+                config: { fullWidth: true }
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init: function () {
+                sap.ui.getCore().applyTheme("sap_fiori_3");
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // enable routing
+                this.getRouter().initialize();
+
+                // set the device model
+                this.setModel(models.createDeviceModel(), "device");
+            }
+        });
+    }
+);
